1e58ac45218018b587726567fe5148b5b003b2e6
v1b3m <vibenjamin6@gmail.com>
2019-02-25 00:41:22 +0300

    Add cookies

--- WEB-INF/src/Welcome.java
+++ WEB-INF/src/Welcome.java
@@ -2,10 +2,8 @@ import javax.servlet.http.*;
 import javax.servlet.*;
 import java.io.*;
 
-public class Login extends HttpServlet {
+public class Welcome extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        // Get the parameter from the request
-        String username = request.getParameter("username");
 
         // Send the request back to the user
         try {
@@ -13,13 +11,13 @@ public class Login extends HttpServlet {
             PrintWriter writer = response.getWriter();
 
             // add a cookie object that stores the username
-            Cookie ck = new Cookie("uname", username);
+            Cookie ck[] = request.getCookies();
 
             //add cookie to the response
             response.addCookie(ck);
             
             writer.println("<html><body>");
-            writer.println("<h1>Hello "+ username + ", it's working!!</h1>" );
+            writer.println("<h1>Hello "+ ck[0].getValue() + ", it's working!!</h1>" );
             writer.println("</body></html>");
 
             // close the stream
